5156800375bf510482ec20ee6b0ac60f95f22215
superstar0402 <bensonlee033@gmail.com>
2023-11-12 07:06:07 +0100

    contracts trio

--- src/NFTManager.sol
+++ src/NFTManager.sol
@@ -15,26 +15,28 @@ contract NFTManager is IERC721Receiver, Ownable2Step {
     uint256 public immutable threshold = 24 hours;
     uint256 constant public rewardAmount = 10;  
 
-    mapping(address => uint256) public stakedNFT;
-    mapping(address => uint256) public stakedTime;
+    mapping(address => mapping(uint256 => bool)) public stakedNFT;
+    mapping(address => mapping(uint256 => uint256)) public stakedTime;
 
     constructor(address _owner, address _rewardToken) public Ownable(_owner){
          rewardToken = IERC20(_rewardToken);
     }
 
-    function stakeNFT(address nft, uint256 tokenId) external {
+    function stakeNFT(uint256 tokenId) external {
+        require(stakedNFT[msg.sender][tokenId] == false, "Already staked");
+        require(ERC721(nftSpecial).ownerOf(tokenId) == msg.sender, "Invalid Owner");
+        stakedNFT[msg.sender][tokenId] = true;
+        stakedTime[msg.sender][tokenId] = block.timestamp;
         ERC721(nftSpecial).safeTransferFrom(msg.sender, address(this), tokenId);
-        stakedNFT[msg.sender] = tokenId;
-        stakedTime[msg.sender] = block.timestamp;
     }
 
-    function unStakeNFT() external {
-        ERC721(nftSpecial).safeTransferFrom(address(this), msg.sender, stakedNFT[msg.sender]);
+    function unStakeNFT(uint256 tokenId) external {
+        ERC721(nftSpecial).safeTransferFrom(msg.sender, address(this), tokenId);
     }
 
     function claim(uint256 tokenId, uint256 amount) external returns (bool) {
-        require(stakedNFT[msg.sender] == tokenId, "Invalid Owner");
-        require(block.timestamp > stakedTime[msg.sender] + threshold, "Not enough time has passed");
+        require(stakedNFT[msg.sender][tokenId] == true, "Not staked");
+        require(block.timestamp > stakedTime[msg.sender][tokenId] + threshold, "Not enough time has passed");
         IERC20(rewardToken).safeTransferFrom(address(this), msg.sender, rewardAmount);
         return true;
     }
--- test/NFTSpecial.t.sol
+++ test/NFTSpecial.t.sol
@@ -1,43 +1,44 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import {Test, console2} from "forge-std/Test.sol";
-// import {NFTSpecial} from "../src/NFTSpecial.sol";
+import {Test, console2} from "forge-std/Test.sol";
+import {NFTSpecial} from "../src/NFTSpecial.sol";
 
-// contract NFTSpecialTest is Test {
-//     NFTSpecial public nftspecial;
+contract NFTSpecialTest is Test {
+    NFTSpecial public nftspecial;
 
-//     function setUp() public {
-//         address owner = vm.addr(0x1234);
-//         bytes32 merkleRoot = bytes32("0x1234");
-//         nftspecial = new NFTSpecial(owner, merkleRoot);
-//     }
+    function setUp() public {
+        address owner = vm.addr(0x1234);
+        address artist = vm.addr(0x44);
+        bytes32 merkleRoot = bytes32("0x1234");
+        nftspecial = new NFTSpecial(owner, merkleRoot, artist);
+    }
 
-//     function testSupply() public {
-//         assertEq(nftspecial.tokenSupply(), 1);
-//     }
+    function testSupply() public {
+        assertEq(nftspecial.tokenSupply(), 0);
+    }
 
-//     function testSupplyIncrease() public {
-//         address Bob = vm.addr(0x55);
-//         vm.deal(Bob, 2 ether);
-//         vm.prank(Bob);
-//         nftspecial.mint{value: 1 ether}(0);
-//         assertEq(nftspecial.tokenSupply(), 2);
-//     }
+    // function testSupplyIncrease() public {
+    //     address Bob = vm.addr(0x55);
+    //     vm.deal(Bob, 2 ether);
+    //     vm.prank(Bob);
+    //     nftspecial.mint{value: 1 ether}(0);
+    //     assertEq(nftspecial.tokenSupply(), 2);
+    // }
 
-//     function testMintFail() public {
-//         address Bob = vm.addr(0x55);
-//         vm.deal(Bob, 10 ether);
-//         vm.startPrank(Bob);
-//         nftspecial.mint{value: 1 ether}(0);
-//         nftspecial.mint{value: 1 ether}(0);
-//         vm.stopPrank();
-//         assertEq(nftspecial.tokenSupply(), 3);
-//     }
+    // function testMintFail() public {
+    //     address Bob = vm.addr(0x55);
+    //     vm.deal(Bob, 10 ether);
+    //     vm.startPrank(Bob);
+    //     nftspecial.mint{value: 1 ether}(0);
+    //     nftspecial.mint{value: 1 ether}(0);
+    //     vm.stopPrank();
+    //     assertEq(nftspecial.tokenSupply(), 3);
+    // }
 
-//     function testMint() public {
-//         nftspecial.mint(0);
-//         nftspecial.mint(1);
-//         assertEq(nftspecial.tokenSupply(), 2);
-//     }
-// }
+    // function testMint() public {
+    //     nftspecial.mint(0);
+    //     nftspecial.mint(1);
+    //     assertEq(nftspecial.tokenSupply(), 2);
+    // }
+}
